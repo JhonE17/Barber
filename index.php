cbe8bb8e9dd0c8a5d437807fa1954522a42fdfbe
Stiwar9816 <stiwar.asprilla1998@gmail.com>
2018-10-12 21:03:52 -0500

    Prueba

--- index.php
+++ index.php
@@ -3,7 +3,7 @@ include('db.php');
 ?>
 <!DOCTYPE html>
 
-	<!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
+	<!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7"> <![endif] s-->
 	<!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8"> <![endif]-->
 	<!--[if IE 8]>         <html class="no-js lt-ie9"> <![endif]-->
 	<!--[if gt IE 8]><!--> <html class="no-js"> <!--<![endif]-->
