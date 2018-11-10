a7770c0c818df6e2ccb192c661d38cec8913849f
Stiwar98 <stiwar.asprilla1998@gmail.com>
2018-11-10 15:56:19 -0500

    .ico actualizado

--- production/personal.php
+++ production/personal.php
@@ -22,6 +22,7 @@ while ($fila=mysqli_fetch_array($rs)) {
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="icon" href="images/favicon.ico" type="image/ico" />
 
     <title>BarberShopQuib</title>
 
--- production/profile.php
+++ production/profile.php
@@ -23,7 +23,7 @@ while ($fila=mysqli_fetch_array($rs)) {
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-	<link rel="icon" href="images/favicon.ico" type="image/ico" />
+    <link rel="icon" href="images/favicon.ico" type="image/ico" />
 
     <title>Admin BarberShop</title>
 
